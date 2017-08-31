d29b48c627d9ef571f037375df70120717d701af
Paolo <paolo.gava@gmail.com>
2017-08-31 17:26:14 +1000

    Add coin

---
+++ Zimrii.Solidity/contracts/zimco/build.ps1
@@ -0,0 +1,2 @@
+solc -o bin --optimize --overwrite --bin owned.sol tokenRecipient.sol token.sol MyAdvancedToken.sol
+solc -o bin --optimize --overwrite --abi owned.sol tokenRecipient.sol token.sol MyAdvancedToken.sol
---
+++ Zimrii.Solidity/contracts/zimco/tokenRecipient.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.13;
+
+contract tokenRecipient { 
+    function receiveApproval(address from, uint256 value, address token, bytes extraData); 
+}
+
